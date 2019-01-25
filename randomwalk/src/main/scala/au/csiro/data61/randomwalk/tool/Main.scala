516dce2938dc146981ddce5a7a6533d26322d6b8
pptbt29 <pptbt29@gmail.com>
2019-01-25 18:00:55 +0800

    Update Main.scala

--- randomwalk/src/main/scala/au/csiro/data61/randomwalk/tool/Main.scala
+++ randomwalk/src/main/scala/au/csiro/data61/randomwalk/tool/Main.scala
@@ -1,7 +1,10 @@
 package au.csiro.data61.randomwalk.tool
 
+import org.apache.spark.sql.SparkSession
+
 class Main {
   def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().getOrCreate()
     PhoneNumberPairTool.setPhoneNumber2vec("syang/output/number2vec")
     PhoneNumberPairTool.getTopTenSimilarity("18826419798")
   }
