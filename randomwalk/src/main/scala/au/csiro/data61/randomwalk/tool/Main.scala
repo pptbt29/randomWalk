4a64b466b31dbe080395cd70e6ad459a8088c959
pptbt29 <pptbt29@gmail.com>
2019-01-25 17:55:52 +0800

    hard code the phone number in Main

--- randomwalk/src/main/scala/au/csiro/data61/randomwalk/tool/Main.scala
+++ randomwalk/src/main/scala/au/csiro/data61/randomwalk/tool/Main.scala
@@ -3,6 +3,6 @@ package au.csiro.data61.randomwalk.tool
 class Main {
   def main(args: Array[String]): Unit = {
     PhoneNumberPairTool.setPhoneNumber2vec("syang/output/number2vec")
-    PhoneNumberPairTool.getTopTenSimilarity(args(0))
+    PhoneNumberPairTool.getTopTenSimilarity("18826419798")
   }
 }
